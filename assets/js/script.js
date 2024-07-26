054e3fb2e523659b5304998043a0f464625b0c24
lsodini <lucasodini5@gmail.com>
2024-07-26 17:01:56 +0200

    altezza card uguali

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,30 +16,31 @@ function displayProducts(products) {
   productContainer.innerHTML = '';
   products.forEach(product => {
     const productCard = `
-      <div class="col-md-4 mb-4">
-	  <div class="card shadow-sm">
-          <a href="detail.html?productId=${product._id}">
-            <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
-          </a>
-          <div class="card-body oro">
-		   <a href="detail.html?productId=${product._id}" >
-            <h5 class="card-title">${product.name}</h5>
-			 </a>
-            <p class="card-text">${product.description}</p>
-            <p class="card-text"><strong>Brand:</strong> ${product.brand}</p>
-            <p class="card-text"><strong>Price:</strong> ${product.price} crediti</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <a href="backoffice.html?productId=${product._id}" class="btn btn-sm btn-outline-primary">Edit</a>
-                  
-				</div>
-         <div >
-         <a href="#" class="btn-primary btn">BUY</a>
-         	</div>
-            </div>
+     <div class="col-md-4 mb-4 d-flex align-items-stretch">
+  <div class="card shadow-sm d-flex flex-column">
+    <a href="detail.html?productId=${product._id}">
+      <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
+    </a>
+    <div class="card-body oro d-flex flex-column">
+      <a href="detail.html?productId=${product._id}">
+        <h5 class="card-title">${product.name}</h5>
+      </a>
+      <p class="card-text">${product.description}</p>
+      <p class="card-text"><strong>Brand:</strong> ${product.brand}</p>
+      <p class="card-text"><strong>Price:</strong> ${product.price} crediti</p>
+      <div class="mt-auto">
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <a href="backoffice.html?productId=${product._id}" class="btn btn-sm btn-outline-primary">Edit</a>
+          </div>
+          <div>
+            <a href="#" class="btn btn-primary">BUY</a>
           </div>
         </div>
       </div>
+    </div>
+  </div>
+</div>
     `;
     productContainer.innerHTML += productCard;
   });
